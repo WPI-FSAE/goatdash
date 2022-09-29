c7a30790dcd93dbd2098bef69accc82dd0e600cd
Ted Clifford <efclifford@wpi.edu>
2022-09-28 22:38:06 -0400

    React app demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [speed, setSpeed] = useState(0);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8000');
+      
+    ws.addEventListener('open', (event) => {
+        console.log("opening conn...");
+        ws.send('START');
+    });
+
+    ws.addEventListener('message', (event) => {
+        setSpeed(event.data);
+    });
+
+    ws.addEventListener('close', (event) => {
+        console.log(event);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          This is the Dashboard
+          The car is going {speed} mph.
         </p>
       </header>
     </div>
