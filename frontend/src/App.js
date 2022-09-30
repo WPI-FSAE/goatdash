33fa21cc3f924e0001781456822cd8b07f2cea8c
Ted Clifford <efclifford@wpi.edu>
2022-09-30 15:46:59 -0400

    Implemented power fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,11 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [speed, setSpeed] = useState(0);
-  const [batt, setBatt] = useState(0);
-  const [power, setPower] = useState(0);
+  const [avgCell, setAvgCell] = useState(0);
+  const [maxCell, setMaxCell] = useState(0);
+  const [minCell, setMinCell] = useState(0);
+  const [invVolts, setInvVolts] = useState(0);
+  const [dcAmps, setDcAmps] = useState(0);
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8000');
@@ -18,9 +21,11 @@ function App() {
       let tm = JSON.parse(event.data);  
       console.log('recv tm: ', tm);
       setSpeed(tm['speed']);
-      setBatt(tm['batt']);
-      setPower(tm['power']);
-
+      setAvgCell(tm['avg_cell']);
+      setMaxCell(0);
+      setMinCell(tm['min_cell']);
+      setInvVolts(tm['inv_volts']);
+      setDcAmps(tm['dc_amps']);
     });
 
     ws.addEventListener('close', (event) => {
@@ -40,14 +45,23 @@ function App() {
       </div>
 
       <div id="battery">
+        <p id="avg">
+          Avg Cell: {avgCell}v
+        </p>
         <p>
-          Battery: {batt}%
+          Max Cell: {maxCell}v
+        </p>
+        <p>
+          Min Cell: {minCell}v
         </p>
       </div>
 
       <div id="power">
         <p>
-          Power: {power}w 
+          Sys Voltage: {invVolts}v
+        </p>
+        <p>
+          Sys Amps: {dcAmps}a
         </p>
       </div>
     </div>
