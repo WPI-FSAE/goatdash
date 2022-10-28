627ac3fca92a5fbb1051bab39d7f7d867fca0143
Ted Clifford <efclifford@wpi.edu>
2022-10-28 12:08:27 -0400

    fullscreen only prod

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    fullscreen: true,
+    fullscreen: !isDev,
     webPreferences: {
       nodeIntegration: true,
     },
