f584e74f39c8a510269947cc76626320ecaf1df3
Ted Clifford <efclifford@wpi.edu>
2022-11-02 13:35:07 -0400

    Added configurable options

--- backend/tm_server.py
+++ backend/tm_server.py
@@ -1,18 +1,22 @@
 import websockets
 import asyncio
 import json
-import random
+import configparser
 from parser import Parser
 import can
 from datetime import datetime
 
-can.rc['interface'] = 'socketcan'
-can.rc['channel'] = 'can1'
+# Load config
+config = configparser.ConfigParser()
+config.read('config.ini')
+PORT = int(config['WS']['Port'])
+
+can.rc['interface'] = config['CAN']['Interface']
+can.rc['channel'] = config['CAN']['Channel']
 
 from can.interface import Bus
 
 bus = Bus()
-
 parser = Parser()
 
 speed = 0
@@ -24,23 +28,31 @@ dc_amps = 0
 odometer = 0
 last_time = datetime.utcnow()
 
+# Handle incoming connection from dashboard
 async def handler(websocket):
     async for message in websocket:
         print(f'RECEIVED: {message}')
 
-        if message == 'START':
+        # Only dashboard connections allowed
+        if message == 'START_DASH':
+
+            # Telemetry send loop
             while True:
-                #await asyncio.sleep(0)
                 await websocket.send(f'{await get_tm()}')
 
+# Read message from can bus, update internal state, return full state
 async def get_tm():
     global speed, inv_voltage, avg_cell, min_cell, max_cell, dc_amps, odometer, last_time
 
     msg = bus.recv(.01)
+
     if msg != None:
+
         msgdef = parser.getMsg(msg.arbitration_id)
+
         if hasattr(msgdef, 'name') and msgdef.name == 'DTI_TelemetryA':
             msg = parser.parse(msg)
+
             speed = msg.ERPM * 0.0015763099 # erpm to mph
             inv_voltage = msg.inputVoltage
             nowtime = datetime.utcnow()
@@ -50,9 +62,11 @@ async def get_tm():
             speed = round(speed, 1)
         elif hasattr(msgdef, 'name') and msgdef.name == 'DTI_TelemetryB':
             msg = parser.parse(msg)
+
             dc_amps = round(msg.DCDeciAmps / 10.0, 1)
         elif hasattr(msgdef, 'name') and msgdef.name == 'BMS_Information' and msgdef.schema.length == len(msg.data):
             msg = parser.parse(msg)
+            
             avg_cell = round((msg.AvgCellVoltage * 0.01) + 2, 2)
             min_cell = round((msg.MinCellVoltage * 0.01) + 2, 2)
             max_cell = round((msg.MaxCellVoltage * 0.01) + 2, 2)
