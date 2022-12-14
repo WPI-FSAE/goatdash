3f83979adf0aaf7c59dceff7b3c030b20974fa5f
Ted Clifford <efclifford@wpi.edu>
2022-12-13 23:06:10 -0500

    Changed speedometer gauge

--- frontend/src/Components/Speedometer.js
+++ frontend/src/Components/Speedometer.js
@@ -42,21 +42,34 @@ const Speedometer = forwardRef((props, ref) => {
     const speedo_left_segments = (speed) => {
       let keys = Array.from(Array(9).keys());
       
-      // Illuminate segments at 5mph intervals
+      // Illuminate segments at 3mph intervals
       return keys.map((key) => {
         let color = 'inherit';
-        if ((8 - key) * 5 < speed) {
-          
-          if (speed <= 70) color = 'var(--positive)';
+        let height = '15px';
+      
+        if ((8 - key) * 3 < speed) {
+                
+          if (speed <= 62) color = 'var(--positive)';
           else color = 'var(--negative)'
 
           // color = 'var(--positive)';
+
+          if ((8 - key) * 3 + 1 > speed) height = '5px';
+          else if ((8 - key) * 3 + 2 > speed) height = '10px';
         }
 
-        return <div className="left-segment" key={key} style={{top: `${key * 12}%`, 
-                                                               backgroundColor: color,
-                                                               boxShadow: color === 'inherit' ? "none" : `0px 0px 50px ${color}`,
-                                                               borderBottomColor: color === 'inherit' ? "" : color}}/>
+        return (
+          <div className="left-segment" key={key} style={{top: `${key * 12}%`}}>
+            <div style={{
+              position: 'absolute',
+              bottom: 0,
+              backgroundColor: color,
+              height: height,
+              width: '100%',
+              boxShadow: color === 'inherit' ? "none" : `0px 0px 50px ${color}`,
+            }}/>
+          </div>          
+        );                                                  
       });
     }
 
@@ -67,9 +80,10 @@ const Speedometer = forwardRef((props, ref) => {
       // Illumincate segments at 5mph intervals
       return keys.map((key) => {
         let color = 'inherit';
-        if ((key * 5) + 45 < speed) {
+        let width = '12%'
+        if ((key * 5) + 27 < speed) {
 
-          if (speed <= 70) color = 'var(--positive)';
+          if (speed <= 62) color = 'var(--positive)';
           else color = 'var(--negative)'
 
           // if (key >= 4) {
@@ -78,9 +92,16 @@ const Speedometer = forwardRef((props, ref) => {
           //   color = 'var(--positive)';
           // }
 
+          if ((key * 5) + 27 + 1 > speed) width ='2.4%';
+          else if ((key * 5) + 27 + 2 > speed) width ='4.8%';
+          else if ((key * 5) + 27 + 3 > speed) width ='7.2%';
+          else if ((key * 5) + 27 + 4 > speed) width ='9.6%';
+          else if ((key * 5) + 27 + 5 > speed) width ='12%';
+
         }
 
         return <div className="top-segment" key={key} style={{left: `${key * 14}%`, 
+                                                              width: width,
                                                               backgroundColor: color,
                                                               boxShadow: color === 'inherit' ? "none" : `0px 0px 50px ${color}`,
                                                               borderBottomColor: color === 'inherit' ? "" : color}}/>
@@ -97,15 +118,19 @@ const Speedometer = forwardRef((props, ref) => {
           
           <div id="amps">
             <h2 id="amp-text"><i>{padAmps(dcAmps)}</i></h2> 
-            <span id="label"><i> AMPS</i></span>    
+            <span id="label"><i> A</i></span>    
           </div>
 
           <div id="peaks">
             <div style={{float: "right"}}><i>
-            {peakAmps}A<span style={{color: 'var(--negative)'}}> ⇝</span>
+              <span style={{fontFamily: 'var(--main-font)', fontSize: '1.5rem'}}>{peakAmps}</span>
+              <span style={{fontSize: '1rem'}}>A</span>
+              <span style={{color: 'var(--negative)'}}> ⇝</span>
               </i></div>
             <div style={{float: "left"}}><i>
-              <span style={{color: 'var(--positive)'}}>⇜ </span>{peakRegen}A
+              <span style={{color: 'var(--positive)'}}>⇜ </span>
+              <span style={{fontFamily: 'var(--main-font)', fontSize: '1.5rem'}}>{peakRegen}</span>
+              <span style={{fontSize: '1rem'}}>A</span>
             </i></div>
           </div>
 
