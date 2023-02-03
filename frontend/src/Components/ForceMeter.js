7aeac3c8d10f35e7a1997322c178b08f0df2af58
Ted Clifford <efclifford@wpi.edu>
2023-02-03 11:42:08 -0500

    Changes to max force value styling

--- frontend/src/Components/ForceMeter.js
+++ frontend/src/Components/ForceMeter.js
@@ -4,9 +4,9 @@ import { useState, forwardRef, useImperativeHandle } from 'react';
 function padDecimal(val) {
     let val_str = val.toString();
     if (val_str.includes('.')) {
-        if (val_str.indexOf('.') + 2 === val_str.length) val_str = val_str + '0';
+        if (val_str.indexOf('.') + 1 === val_str.length) val_str = val_str + '0';
     }
-    else val_str = val_str + ".00";
+    else val_str = val_str + ".0";
 
     return val_str;
 }
@@ -22,22 +22,25 @@ const ForceMeter = forwardRef((props, ref) => {
 
     useImperativeHandle(ref, () => ({
         updateForceMeter(tm) {
-           
+            if (tm['f_x'] !== undefined && tm['f_x'] !== posX) setPosX(tm['f_x']);
+            if (tm['f_y'] !== undefined && tm['f_y'] !== posY) setPosY(tm['f_y']);
+
+            if (tm['max_fr'] !== undefined && tm['max_fr'] !== maxFr) setMaxFr(tm['max_fr']);
+            if (tm['max_rr'] !== undefined && tm['max_rr'] !== maxRr) setMaxRr(tm['max_rr']);
+            if (tm['max_lt'] !== undefined && tm['max_lt'] !== maxLt) setMaxLt(tm['max_lt']);
+            if (tm['max_rt'] !== undefined && tm['max_rt'] !== maxRt) setMaxRt(tm['max_rt']);
         }
     }));
 
     const getCoords = (x, y) => {
-        let hyp = (x**2 + y**2);
+        let hyp = Math.sqrt(x**2 + y**2);
 
         if (hyp > 1) {
             let scalar = 1 / hyp;
-            return {x: Math.sqrt(scalar) * x, y: Math.sqrt(scalar) * y};
+            return {x: scalar * x, y: scalar * y};
         }
 
         return {x: x, y: y};
-        
-
-
     } 
 
     const coords = getCoords(posX, posY);
@@ -66,6 +69,8 @@ const ForceMeter = forwardRef((props, ref) => {
             <p className="force-val" id="rr-max">{padDecimal(maxRr)}</p>
             <p className="force-val" id="lt-max">{padDecimal(maxLt)}</p>
             <p className="force-val" id="rt-max">{padDecimal(maxRt)}</p>
+
+            <p id="force-label">Max G</p>
             
         </div>
     )
