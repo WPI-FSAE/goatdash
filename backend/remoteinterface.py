1c3041ea4664e26627635268d174dedc90b3c553
Galputer <eakelley2001@gmail.com>
2023-03-21 09:57:54 -0400

    remoteTelemetry
    removed states and prioritization

--- backend/remoteinterface.py
+++ backend/remoteinterface.py
@@ -6,12 +6,6 @@
 import asyncio
 import json
 
-# Frontend state
-REMOTE = 0
-GPS = 1
-CHARGE = 2
-DEBUG = 3
-
 class RemoteInterface:
 
     def __init__(self, vehicle, logger, race, refresh=60):
@@ -19,20 +13,6 @@ class RemoteInterface:
         self.dbg = logger
         self.race = race
         self.refresh = refresh
-        self.state = REMOTE
-
-    async def transmit(self, websocket):
-        inc = 4
-        tick = 1
-
-        while tick > 0:
-            await websocket.send(json.dumps({'speed': tick * .6}))
-            await asyncio.sleep(.01)
-
-            if tick > 100:
-                inc = -4
-
-            tick += inc
         
     async def send_tm(self, websocket):
         """
@@ -41,62 +21,43 @@ class RemoteInterface:
 
         i = 0
 
-        await self.transmit(websocket)
-
         while True:
             pkt = {}
 
             # Packet type switching (allows for some values to updated faster than others)
-            if (i % 2 == 0):
-                
-                pkt = {**pkt, **{'rpm': self.vic.erpm // 2, 
-                                'speed': round(self.vic.speed, 1), 
-                                'inv_volts': self.vic.inv_voltage,
-                                'dc_amps': self.vic.amps}}
-
-            if (self.state == REMOTE):
-                if (i % 2 == 1):
-                    pkt = {**pkt, **{'race_time': round(self.race.get_race_time() * 1000),
-                                     'lap_time': round(self.race.get_lap_time() * 1000),
-                                     'lap_use': round(self.race.get_lap_volt(), 2),
-                                     'f_x': self.vic.accel_x,
-                                     'f_y': self.vic.accel_y}}
-                if (i == 1):
-                    pkt = {**pkt, **{'avg_cell': self.vic.cell_voltages["avg"],
-                                    'min_cell': self.vic.cell_voltages["min"], 
-                                    'max_cell': self.vic.cell_voltages["max"],
-                                    'acc_temp': self.vic.temps["acc"], 
-                                    'inv_temp': self.vic.temps["inv"],
-                                    'mtr_temp': self.vic.temps["mtr"],
-                                    'odometer': round(self.vic.odometer, 1), 
-                                    'trip': round(self.vic.trip, 3), 
-                                    'rtd': self.vic.rtd, 
-                                    'fault': self.vic.fault,
-                                    'peak_amps': self.vic.amps_max["draw"],
-                                    'peak_regen': self.vic.amps_max["regen"],
-                                    'mi_est': self.vic.range_est["mi"],
-                                    'lap_est': self.vic.range_est["lap"],
-                                    'time_est': self.vic.range_est["time"],
-                                    'batt_pct': self.vic.batt_pct,
-                                    'max_fr': round(self.vic.accel_max["fr"], 1),
-                                    'max_rr': round(self.vic.accel_max["rr"], 1),
-                                    'max_lt': round(self.vic.accel_max["lt"], 1),
-                                    'max_rt': round(self.vic.accel_max["rt"], 1)}}
-
-            elif (self.state == GPS):
-                if (i == 1):
-                    pkt = {**pkt, **{'lat': self.vic.lat,
-                                     'long': self.vic.long,
-                                     'lap_armed': self.race.is_ready()}}
-
-            elif (self.state == DEBUG):
-                if (self.dbg.msg_avail()):
-                    pkt = {**pkt, **{'dbg_msgs': self.dbg.get_msg()}}
-
-            if (i >= 9):
-                i = 0
-            else:
-                i += 1
+            pkt = {**pkt, **{'rpm': self.vic.erpm // 2, 
+                            'speed': round(self.vic.speed, 1), 
+                            'inv_volts': self.vic.inv_voltage,
+                            'dc_amps': self.vic.amps,
+                            'race_time': round(self.race.get_race_time() * 1000),
+                            'lap_time': round(self.race.get_lap_time() * 1000),
+                            'lap_use': round(self.race.get_lap_volt(), 2),
+                            'f_x': self.vic.accel_x,
+                            'f_y': self.vic.accel_y,
+                            'avg_cell': self.vic.cell_voltages["avg"],
+                            'min_cell': self.vic.cell_voltages["min"], 
+                            'max_cell': self.vic.cell_voltages["max"],
+                            'acc_temp': self.vic.temps["acc"], 
+                            'inv_temp': self.vic.temps["inv"],
+                            'mtr_temp': self.vic.temps["mtr"],
+                            'odometer': round(self.vic.odometer, 1), 
+                            'trip': round(self.vic.trip, 3), 
+                            'rtd': self.vic.rtd, 
+                            'fault': self.vic.fault,
+                            'peak_amps': self.vic.amps_max["draw"],
+                            'peak_regen': self.vic.amps_max["regen"],
+                            'mi_est': self.vic.range_est["mi"],
+                            'lap_est': self.vic.range_est["lap"],
+                            'time_est': self.vic.range_est["time"],
+                            'batt_pct': self.vic.batt_pct,
+                            'max_fr': round(self.vic.accel_max["fr"], 1),
+                            'max_rr': round(self.vic.accel_max["rr"], 1),
+                            'max_lt': round(self.vic.accel_max["lt"], 1),
+                            'max_rt': round(self.vic.accel_max["rt"], 1),
+                            'lat': self.vic.lat,
+                            'long': self.vic.long,
+                            'lap_armed': self.race.is_ready(),
+                            'dbg_msgs': self.dbg.get_msg()}}
             
             await websocket.send(json.dumps(pkt))
             await asyncio.sleep(1 / self.refresh)    # Define frontend refresh rate
